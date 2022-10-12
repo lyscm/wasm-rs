6fa01bc45fe25658bc4e79c5859fce5b4b502ef9
lyscm <no-reply@lyscm.com>
2022-10-12 16:15:35 +0000

    reduce Container size

--- src/utils.rs
+++ src/utils.rs
@@ -3,5 +3,5 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen(module = "/src/js/time.js")]
 extern "C" {
     #[wasm_bindgen]
-    pub fn get_now_date() -> String;
+    pub fn _get_now_date() -> String;
 }
